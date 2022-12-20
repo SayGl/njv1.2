ae58d1451c7e4d0598b258581ca70b09980b6e2b
Say GL <glaze1236@gmail.com>
2022-12-21 00:36:15 +0300

    пифагор думает над сыром луком и котлетой

--- src/SirLukIliKotleta.java
+++ src/SirLukIliKotleta.java
@@ -2,11 +2,15 @@ public class SirLukIliKotleta {
     public static void main() {
         System.out.println("соболев придумывает песню про свой бургер!");
         int money = 100;
-        if (money <= 100) {
+        boolean burger1 = money <= 100;
+        boolean burger2 = money > 100 && money < 200;
+        if (burger1) {
             System.out.println("сыр");
-        } else if (money > 100 || money < 200) {
+        }
+        if (burger2) {
             System.out.println("лук");
-        } else if (money > 200) {
+        }
+        if (!burger1 && !burger2) {
             System.out.println("котлета");
 
         }
