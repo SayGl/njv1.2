db3bc07eced33a5d12d635afe0c97ece2cd33e70
Say GL <glaze1236@gmail.com>
2022-12-20 16:11:13 +0300

    заходит как-то мужик в бар...

---
+++ src/Jumoreska.java
@@ -0,0 +1,10 @@
+public class Jumoreska {
+    public static void main(String[] args) {
+        float a = 2;
+        while (a!=0) {
+            a = a/2;
+            System.out.println ("Заходит мужик в бар и говорит: налейте мне "+ a + " кружку пива ");
+        }
+        System.out.println ("А бармен отвечает: да вам и двух кружек на всех хватит ");
+    }
+}
