b61c77c9bfd16d8a58f8b80c17b8a65a1fbb16a7
Say GL <glaze1236@gmail.com>
2022-12-21 01:19:29 +0300

    эрик?..

---
+++ src/sr.java
@@ -0,0 +1,10 @@
+public class sr {
+    public static void main(String[] args) {
+        int j = 100;
+        for (j = 100; j < 1000; j++) {
+            if (j % 2 == 1 && j % 5 == 0) {
+                System.out.println(j);
+            }
+        }
+    }
+}
